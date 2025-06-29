8204b4815c0897d98d1c13973f9185b486be4966
Deak Daniel <83765724+deak-daniel@users.noreply.github.com>
2025-06-29 23:48:33 +0200

    parallax added, every grif changed to flexbox

--- js/app.js
+++ js/app.js
@@ -2,8 +2,6 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if(entry.isIntersecting){
             entry.target.classList.add("show");
-        }else{
-            entry.target.classList.remove("show");
         }
     });
 });
@@ -13,3 +11,18 @@ console.log(hiddenElements);
 for (let index = 0; index < hiddenElements.length; index++) {
     observer.observe(hiddenElements[index]);
 }
+
+window.addEventListener("scroll", function(event){
+
+		var top = this.pageYOffset;
+
+		var layers = document.getElementsByClassName("parallax");
+		var layer, speed, yPos;
+		for (var i = 0; i < layers.length; i++) {
+			layer = layers[i];
+			speed = layer.getAttribute('data-speed');
+			var yPos = -(top * speed / 100);
+			layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
+
+		}
+	});
